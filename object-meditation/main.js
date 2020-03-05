50dc4edb4a1ceb928634330b6f0165b1819b1b22
Frederic Chen <fredericchen@Frederics-MacBook-Pro.local>
2020-03-05 16:12:23 -0500

    push

---
+++ object-meditation/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+// function open() {
+//   $("#first_img").css("display", "block");
+//
+// };
+//
+// function close() {
+//     $("#first_img").css("display", "none");
+//   };
+
+  $("#first").click(function() {
+      $("#first_img").css("display", "block");
+   });
+  $("#first_img").click(function() {
+       $("#first_img").css("display", "none");
+    });
+});
